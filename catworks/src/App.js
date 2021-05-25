2fdf0f1f4cecf5b1f1c9853f3f6df9e22b5407de
Mario N <butmail02@gmail.com>
2021-05-25 13:05:12 +0200

    Init Project

---
+++ catworks/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Post from './component/Post'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Title</h1> */}
+      <Post />
+    </div>
+  );
+}
+
+export default App;
